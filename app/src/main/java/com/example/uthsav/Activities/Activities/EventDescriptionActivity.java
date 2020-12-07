51ceeb655528e112ce3cf1c8e3d5138b054fd196
Kavya-Bhat <68507147+Kavya-Bhat@users.noreply.github.com>
2020-12-07 16:44:34 +0530

    minor changes done in activity_login.xml and activity_event_description.xml basic done

---
+++ app/src/main/java/com/example/uthsav/Activities/Activities/EventDescriptionActivity.java
@@ -0,0 +1,26 @@
+package com.example.uthsav.Activities.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.uthsav.R;
+
+public class EventDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_description);
+    }
+
+    public void onClickMessage(View view)
+    {
+    }
+
+    public void onClickBuyTicket(View view)
+    {
+
+    }
+}
