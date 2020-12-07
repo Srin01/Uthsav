415ff5f235ef652492de26b8d1cb0f1d57aeaed9
Mohith Varma <mohithvarmais@gmail.com>
2020-12-07 13:19:00 +0530

    Splash and login activity added

---
+++ app/src/main/java/com/example/uthsav/Activities/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.example.uthsav.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.example.uthsav.Activities.Activities.HomeActivity;
+import com.example.uthsav.Activities.Activities.LoginActivity;
+import com.example.uthsav.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final Intent intent;
+        intent = firebaseAuth.getCurrentUser() != null ? new Intent(this, HomeActivity.class) : new Intent(this, LoginActivity.class);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> {
+            startActivity(intent);
+            finish();
+
+        }, 3000);
+
+    }
+}
